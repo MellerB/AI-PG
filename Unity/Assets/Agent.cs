89d8e687babf6a4b9757717e8e05f86db8e89b0a
Paweł Chłąd <pchlad5@gmail.com>
2019-12-19 21:06:46 +0100

    Added gather inputs method to Agent class

--- Unity/Assets/Agent.cs
+++ Unity/Assets/Agent.cs
@@ -10,14 +10,14 @@ public class Agent : MonoBehaviour
 
     //Target cookie jar for every agent that exists
     public static Transform cookieJar;
-
     void Awake()
     {
+        cookieJar = GameObject.Find("cookieJar").transform;
         network = new NetworkModel();
-     //   network.Layers.Add(new NeuralLayer(9, 0.1, "INPUT"));
-       // network.Layers.Add(new NeuralLayer(15, 0.1, "HIDDEN"));
-      //  network.Layers.Add(new NeuralLayer(4, 0.1, "OUTPUT"));
-      //  network.Build();
+        network.Layers.Add(new NeuralLayer(9, 0.1, "INPUT"));
+        network.Layers.Add(new NeuralLayer(15, 0.1, "HIDDEN"));
+        network.Layers.Add(new NeuralLayer(4, 0.1, "OUTPUT"));
+        network.Build();
     }
 
     void Start()
@@ -27,9 +27,9 @@ public class Agent : MonoBehaviour
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
-        GatherInputs();
+        Debug.Log(GatherInputs()[0]);
     }
 
 
@@ -41,15 +41,16 @@ public class Agent : MonoBehaviour
         //1. raycast 8 diffrent rays in 8 diffrent directions and get distance from the walls
         for (int i = -1; i < 2; i++)
         {
-            for (int j = -1; i < 2; j++)
+            for (int j = -1; j < 2; j++)
             {
                 Vector3 dir = new Vector3(i, j, 0);
                 dir.Normalize();
                 RaycastHit hit;
-                Debug.DrawRay(this.transform.position, dir, Color.green, 0.3f);
+           
                 if (Physics.Raycast(this.transform.position, dir, out hit, 100.0f))
                 {
                     results.Add((double)hit.distance/100.0f);
+                    Debug.DrawRay(transform.position, hit.point, Color.green, 0.1f, false);
                 }
                 else 
                 {
@@ -64,7 +65,9 @@ public class Agent : MonoBehaviour
         return results;
     }
 
-
-
 }
+
+
+
+
 
