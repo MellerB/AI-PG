08392e6df6465106d7afc14671c34ba2b4ad7475
Paweł Chłąd <pchlad5@gmail.com>
2020-02-06 22:59:45 +0100

    eow for today

--- Unity/Assets/CameraController.cs
+++ Unity/Assets/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,29 +9,52 @@ public class CameraController : MonoBehaviour
     public Transform target;
     public Vector3 offset;
 
+    private Vector3 mouseDelta = new Vector2(0f, 0f);
+    private Vector3 previousMousePos;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        previousMousePos = Input.mousePosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.position - offset;
 
+        if (Input.GetButton("BreakTarget"))
+            target = null;
+
+        if (Input.GetButton("BestTarget"))
+            target = ChooseBestTarget();
+
+
+        if (target == null)
+        {
+            transform.Translate(Vector3.forward * Input.GetAxis("Forward"));
+            transform.Translate(Vector3.up * Input.GetAxis("Up"));
+            transform.Translate(Vector3.right * Input.GetAxis("Right"));
+            mouseDelta = Input.mousePosition - previousMousePos;
+
+            transform.rotation *= Quaternion.AngleAxis(mouseDelta.x, Vector3.up);
+            transform.rotation *= Quaternion.AngleAxis(mouseDelta.y, Vector3.forward);            
+            
+        }
+
+        previousMousePos = Input.mousePosition;
+
+        if (target != null)
+            transform.position = target.position - offset;
         //Check for input
-                
-
-
-
-
 
     }
 
 
-    
 
 
+    Transform ChooseBestTarget()
+    {
+        throw new NotImplementedException();
+    }
 
 }
--- Unity/Assets/InputMonitor.cs
+++ Unity/Assets/InputMonitor.cs
@@ -13,25 +13,29 @@ public class InputMonitor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Target == null)
+            Debug.LogWarning("Target is null, no data will be shown.");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        string input_data = "";
-        string output_data = "";
-        for (int i = 0; i < Target.lastInputs.Count; i++)
+        if (Target != null)
         {
-            input_data += "Input " + i + " --- " + Target.lastInputs[i] + "\n";
+            string input_data = "";
+            string output_data = "";
+            for (int i = 0; i < Target.lastInputs.Count; i++)
+            {
+                input_data += "Input " + i + " --- " + Target.lastInputs[i] + "\n";
+            }
+
+            for (int i = 0; i < Target.lastOutputs.Count; i++)
+            {
+                output_data += "Output " + i + " --- " + Target.lastOutputs[i] + "\n";
+            }
+
+            text.text = input_data + output_data;
         }
 
-        for (int i = 0; i < Target.lastOutputs.Count; i++)
-        {
-            output_data += "Output " + i + " --- " + Target.lastOutputs[i] + "\n";
-        }
-
-
-        text.text = input_data + output_data;
     }
 }
